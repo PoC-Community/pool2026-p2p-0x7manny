b5c716deaa0fa410d8ecf14797765e34416e9067
manny <115085160+manny-pro@users.noreply.github.com>
2026-02-02 11:42:01 +0100

    fix: change createprofile condition

--- src/ProfileSystem.sol
+++ src/ProfileSystem.sol
@@ -2,20 +2,12 @@
 pragma solidity ^0.8.20;
 
 contract ProfileSystem {
-    // ========== ENUMS ==========
-    // TODO: Create enum Role { GUEST, USER, ADMIN }
+
     enum Role {
         GUEST,
         USER,
         ADMIN
     }
-
-    // ========== STRUCTS ==========
-    // TODO: Create struct UserProfile with:
-    //   - string username
-    //   - uint256 level
-    //   - Role role
-    //   - uint256 lastUpdated
 
     struct UserProfile {
         string username;
@@ -24,15 +16,8 @@ contract ProfileSystem {
         uint256 lastUpdated;
     }
 
-
-    // ========== MAPPINGS ==========
-    // TODO: mapping(address => UserProfile) public profiles
     mapping(address => UserProfile) public profiles;
 
-    // ========== CUSTOM ERRORS ==========
-    // TODO: error UserAlreadyExists()
-    // TODO: error EmptyUsername()
-    // TODO: error UserNotRegistered()
     error UserAlreadyExists();
     error EmptyUsername();
     error UserNotRegistered();
@@ -48,18 +33,19 @@ contract ProfileSystem {
     event LevelUp(address indexed user, uint256 newLevel);
 
     function createProfile(string calldata _name) external {
-    // TODO: Implement
-        if(bytes(_name).length > 0) {
+        if (bytes(_name).length == 0) {
             revert EmptyUsername();
         }
-        if(profiles[msg.sender].level == 0) {
+
+        if (profiles[msg.sender].level != 0) {
             revert UserAlreadyExists();
         }
+
         profiles[msg.sender] = UserProfile({
-        username: _name,
-        level: 1,
-        role: Role.USER,
-        lastUpdated: block.timestamp
+            username: _name,
+            level: 1,
+            role: Role.USER,
+            lastUpdated: block.timestamp
         });
 
         emit ProfileCreated(msg.sender, _name);
@@ -67,8 +53,8 @@ contract ProfileSystem {
 
     function levelUp() external onlyRegistered {
         profiles[msg.sender].level += 1;
-        profiles[msg.sender].lastUpdated = block.timestamp; 
+        profiles[msg.sender].lastUpdated = block.timestamp;
+
         emit LevelUp(msg.sender, profiles[msg.sender].level);
     }
-
 }
